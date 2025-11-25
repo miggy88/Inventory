ad85e609e8bf30d6df2e33c7a3f8022d19b4bfff
miggy88 <cunananluismiguel11@gmail.com>
2025-11-26 01:13:23 +0800

    ww

--- Inventory/frmAddProduct.cs
+++ Inventory/frmAddProduct.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -23,15 +24,31 @@ namespace Inventory
 
         public frmAddProduct()
         {
-           
-             
-        
-
-
             InitializeComponent();
-
-
         }
+
+        public string Product_Name(string name)
+        {
+            if (!Regex.IsMatch(name, @"^[a-zA-Z]+$"))
+                throw new
+                    ArgumentException("Invalid product name! ");
+                return name;
+        }
+        public int Quantity(string qty)
+        {
+            if (!Regex.IsMatch(qty, @"^[0-9]"))
+                throw new
+                ArgumentException("Invalid product Quantity! ");
+            return Convert.ToInt32(qty);
+        }
+        public double SellingPrice(string price)
+        {
+            if (!Regex.IsMatch(price.ToString(), @"^(\d*\.)?\d+$"))
+                throw new 
+                    ArgumentException("Invalid product price! ");
+            return Convert.ToDouble(price);
+        }
+
 
         private void label2_Click(object sender, EventArgs e)
         {
