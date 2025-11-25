8b9d1c1630a01d521504beeff7b448a07e0454da
miggy88 <cunananluismiguel11@gmail.com>
2025-11-26 01:46:13 +0800

    wefgg

---
+++ Inventory/CustomExeptionClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory
+{
+    public class CustomExeptionClass : Exception
+    {
+      
+
+public class NumberFormatException : Exception
+    {
+        public NumberFormatException(string varName) : base($"Invalid number format for: {varName}") { }
+    }
+
+    public class StringFormatException : Exception
+    {
+        public StringFormatException(string varName) : base($"Invalid string format for: {varName}") { }
+    }
+
+    public class CurrencyFormatException : Exception
+    {
+        public CurrencyFormatException(string varName) : base($"Invalid currency format for: {varName}") { }
+    }
+        
+
+        }
+
+
+    }
